8c0babd68c39bf8004453fdcbca48c19d1ee4970
aryaprayogi16 <aryaprayogi16@gmail.com>
2025-06-11 14:30:51 +0700

    commit arya

--- app/Models/ComputerSkill.php
+++ app/Models/ComputerSkill.php
@@ -1,10 +1,39 @@
-<?php
-
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ComputerSkill extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'candidate_id',
+        'skill_type',
+        'skill_description',
+        'proficiency_level'
+    ];
+
+    // Relationships
+    public function candidate()
+    {
+        return $this->belongsTo(Candidate::class);
+    }
+
+    // Scopes
+    public function scopeByType($query, $type)
+    {
+        return $query->where('skill_type', $type);
+    }
+
+    public function scopeHardware($query)
+    {
+        return $query->where('skill_type', 'Hardware');
+    }
+
+    public function scopeSoftware($query)
+    {
+        return $query->where('skill_type', 'Software');
+    }
 }
